ab4abdfd92dec05e84f81daf0e11329773401c77
Henrique1792 <freitash1792@gmail.com>
2017-05-21 13:58:31 -0300

    Bin conversion fixed

--- supplies.py
+++ supplies.py
@@ -9,85 +9,65 @@ import binascii
 
 class Supplies(object):
     rotationTable=None
+    roundCounter=None 
     def __init__(self):
         self.setRotationTable()
+        self.roundCounter=0
 
     def setRotationTable(self):
         self.rotationTable=[[0 for x in range(8)] for y in range(8)]
         
-        rotationTable[0][0]=55
-        rotationTable[0][1]=43
-        rotationTable[0][2]=37
-        rotationTable[0][3]=40
-        rotationTable[0][4]=16
-        rotationTable[0][5]=22
-        rotationTable[0][6]=38
-        rotationTable[0][7]=12
+        self.rotationTable[0][0]=38
+        self.rotationTable[0][1]=30
+        self.rotationTable[0][2]=50
+        self.rotationTable[0][3]=53
         
-        rotationTable[1][0]=25
-        rotationTable[1][1]=25
-        rotationTable[1][2]=46
-        rotationTable[1][3]=13
-        rotationTable[1][4]=14
-        rotationTable[1][5]=13
-        rotationTable[1][6]=52
-        rotationTable[1][7]=57
+        self.rotationTable[1][0]=48
+        self.rotationTable[1][1]=20
+        self.rotationTable[1][2]=43
+        self.rotationTable[1][3]=31
         
-        rotationTable[2][0]=33
-        rotationTable[2][1]=8
-        rotationTable[2][2]=18
-        rotationTable[2][3]=57
-        rotationTable[2][4]=21
-        rotationTable[2][5]=12
-        rotationTable[2][6]=32
-        rotationTable[2][7]=54
+        self.rotationTable[2][0]=34
+        self.rotationTable[2][1]=14
+        self.rotationTable[2][2]=15
+        self.rotationTable[2][3]=27
         
-        rotationTable[3][0]=34
-        rotationTable[3][1]=43
-        rotationTable[3][2]=25
-        rotationTable[3][3]=60
-        rotationTable[3][4]=44
-        rotationTable[3][5]=9
-        rotationTable[3][6]=59
-        rotationTable[3][7]=34
-        
-        rotationTable[4][0]=28
-        rotationTable[4][1]=7
-        rotationTable[4][2]=47
-        rotationTable[4][3]=48
-        rotationTable[4][4]=51
-        rotationTable[4][5]=9
-        rotationTable[4][6]=35
-        rotationTable[4][7]=41
-        
-        rotationTable[5][0]=17
-        rotationTable[5][1]=6
-        rotationTable[5][2]=18
-        rotationTable[5][3]=25
-        rotationTable[5][4]=43
-        rotationTable[5][5]=42
-        rotationTable[5][6]=40
-        rotationTable[5][7]=15
-        
-        rotationTable[6][0]=58
-        rotationTable[6][1]=7
-        rotationTable[6][2]=32
-        rotationTable[6][3]=45
-        rotationTable[6][4]=19
-        rotationTable[6][5]=18
-        rotationTable[6][6]=2
-        rotationTable[6][7]=56
-        
-        rotationTable[7][0]=47
-        rotationTable[7][1]=49
-        rotationTable[7][2]=27
-        rotationTable[7][3]=58
-        rotationTable[7][4]=37
-        rotationTable[7][5]=48
-        rotationTable[7][6]=53
-        rotationTable[7][7]=56
+        self.rotationTable[3][0]=26
+        self.rotationTable[3][1]=12
+        self.rotationTable[3][2]=58
+        self.rotationTable[3][3]=7
+           
+        self.rotationTable[4][0]=33
+        self.rotationTable[4][1]=49
+        self.rotationTable[4][2]=8
+        self.rotationTable[4][3]=42
+         
+        self.rotationTable[5][0]=39
+        self.rotationTable[5][1]=27
+        self.rotationTable[5][2]=41
+        self.rotationTable[5][3]=14
+         
+        self.rotationTable[6][0]=29
+        self.rotationTable[6][1]=26
+        self.rotationTable[6][2]=11
+        self.rotationTable[6][3]=9
+         
+        self.rotationTable[7][0]=33
+        self.rotationTable[7][1]=51
+        self.rotationTable[7][2]=39
+        self.rotationTable[7][3]=35
+
+    def getBinString(self, param):
+        return bin(int(binascii.hexlify(param.encode()), 16))
 
 
+    def getBlockList(self, bString):
+        rt=None
+        rt=[bString[i:i+64] for i in range(0, len(bString), 64)]
+        return rt
 
-    def getBinString(self, string):
-        return bin(int(binascii.hexlify(string),16))
+    def xorAll(self, strList):
+        rt=strList[0]
+        for i in range(1, len(strList)):
+            rt^=i
+        return rt
